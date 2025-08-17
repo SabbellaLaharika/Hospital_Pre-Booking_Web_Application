c03e5583c24c84b779a5f509f7b69cac518bd395
Sabbella Laharika <22A91A05J9@aec.edu.in>
2025-08-17 14:28:55 +0530

    finaly url update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Hospital_Pre-Booking_Web_Application',
+  base: '/Hospital_Pre-Booking_Web_Application',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
